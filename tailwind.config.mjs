4bdc53551bb83b5fac48ed2a66c9b216ff5ac750
Rachhen <rachhen.it@outlook.com>
2024-01-20 23:14:53 +0700

    added formatting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#4CAF50',
+				secondary: '#087F23',
+				dark: '#333333',
+				light: '#F1F2F4',
+				n: {
+					0: '#FFFFFF',
+					20: '#F1F2F4',
+					60: '#C3C7CB',
+					80: "#A3ABB2",
+					300: '#6D6D6D',
+					500: '#555555',
+					700: '#171F26',
+					900: '#222222',
+				}
+			},
+
+		},
 	},
 	plugins: [],
 }
